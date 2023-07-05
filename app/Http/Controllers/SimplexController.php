eade0f50e93ba237c54603c069dc6c993e1bf868
matheusssousa <matheussousab2014@gmail.com>
2023-07-05 14:33:11 -0300

    metodo minimizar

--- app/Http/Controllers/SimplexController.php
+++ app/Http/Controllers/SimplexController.php
@@ -69,6 +69,7 @@ class SimplexController extends Controller
 
         if($simplex['objetivo'] == "min"){
             foreach ($simplex['funcao'] as $key => $value) {
+                $simplex['funcao'][$key] *= -1;
             }
         }
 
